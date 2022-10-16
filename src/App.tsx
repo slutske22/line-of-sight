6ea4e43c8a2050df5792a7c5cb55a905d2898e62
Seth Lutske <seth.lutske@gmail.com>
2022-10-15 21:29:24 -1000

    Getting somewhere with mapbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,6 @@
 import React from "react";
-import "./App.css";
+import { Map } from "components";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-}
-
-export default App;
+export const App: React.FC = () => {
+	return <Map />;
+};
